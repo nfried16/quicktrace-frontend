59144a7685bd59ab63eaa7dc8340b59588c17baf
Nathaniel Friedman <nathaniel.friedman@duke.edu>
2020-12-06 12:30:05 -0500

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,13 +5,15 @@ import Logo from '../files/LogoWhite.png';
 
 const NavBar = () => {
     return (
-        <div style={{ height: window.innerHeight * .12, width: window.innerWidth, marginBottom: 20 }}>
-            <div style={{ height: window.innerHeight * .12, width: window.innerWidth, position: 'fixed', background: '#01184F', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', zIndex: 100 }}>
-                <Link to='/home' style={{ marginLeft: 50, height: '10vh', width: '10vh' }}>
-                    <Image src={Logo} />
-                </Link>
-                <div style={{ marginLeft: -660, display: 'flow', flexDirection: 'row', marginTop: 10 }}>
-                    <h1 style={{ color: "white", font: "Verdana" }}>QuickTrace</h1>
+        <div style={{ height: window.innerHeight * .12, width: '100vw', marginBottom: 20 }}>
+            <div style={{ height: window.innerHeight * .12, width: '100%', position: 'fixed', background: '#01184F', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', zIndex: 100 }}>
+                <div style = {{marginLeft: 50, height: '10vh', width: '10vh', display: 'flex', flexDirection: 'row'}}> 
+                    <Link to='/home' style={{ margin: '0px 15px'}}>
+                        <Image src={Logo} width = '10vh' height = '10vh' />
+                    </Link>
+                    <div style={{ margin: '0px 15px', display: 'flex', alignItems: 'center'}}>
+                        <h1 style={{ margin: 0, color: "white", font: "Verdana" }}>QuickTrace</h1>
+                    </div>
                 </div>
                 <div style={{ marginRight: 100, display: 'flex', flexDirection: 'row' }}>
                     <NavItem label='Add' link='/add' />
--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -77,4 +77,71 @@ const Survey = () => {
   );
 };
 
+// const Survey = () => {
+//   const onFinish = values => {
+//     console.log('Received values of form:', values);
+//   };
+
+//   return (
+//     <Form name="dynamic_form_item" {...formItemLayout} onFinish={onFinish}>
+//       <Form.Item label = 'Name' name = 'name'>
+//         <Input style={{ width: '60%' }}/>
+//       </Form.Item>
+//       <Form.List
+//         name="addresses"
+//       >
+//         {(fields, { add, remove }, { errors }) => (
+//           <>
+//             {fields.map((field, index) => (
+//               <Form.Item
+//                 {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+//                 label={index === 0 ? 'Addresses' : ''}
+//                 required={false}
+//                 key={field.key}
+//               >
+//                 <Form.Item
+//                   {...field}
+//                   validateTrigger={['onChange', 'onBlur']}
+//                   rules={[
+//                     {
+//                       required: true,
+//                       whitespace: true,
+//                       message: "Please input address or delete this field.",
+//                     },
+//                   ]}
+//                   noStyle
+//                 >
+//                   <Input placeholder="Address" style={{ width: '60%' }} />
+//                 </Form.Item>
+//                 {fields.length > 1 ? (
+//                   <MinusCircleOutlined
+//                     className="dynamic-delete-button"
+//                     onClick={() => remove(field.name)}
+//                   />
+//                 ) : null}
+//               </Form.Item>
+//             ))}
+//             <Form.Item wrapperCol={{ offset: 4 }}>
+//               <Button
+//                 type="dashed"
+//                 onClick={() => add()}
+//                 style={{ width: '60%' }}
+//                 icon={<PlusOutlined />}
+//               >
+//                 Add address
+//               </Button>
+//               <Form.ErrorList errors={errors} />
+//             </Form.Item>
+//           </>
+//         )}
+//       </Form.List>
+//       <Form.Item wrapperCol = {{offset: 4}}>
+//         <Button type="primary" htmlType="submit">
+//           Submit
+//         </Button>
+//       </Form.Item>
+//     </Form>
+//   );
+// };
+
 export default Survey;
