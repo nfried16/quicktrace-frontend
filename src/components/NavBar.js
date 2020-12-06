f29885f4395656ba155d3fbc4e3f66a4716aeb6f
Sebastian Ochoa <sebastian.ochoa@duke.edu>
2020-12-06 00:11:53 -0600

    Improved NavBar and HomePage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Image} from 'antd';
-import Logo from '../files/LOGO-1.png';
+import { Link } from 'react-router-dom';
+import { Image } from 'antd';
+import Logo from '../files/LogoWhite.png';
 
 const NavBar = () => {
-    return(
-        <div style = {{height: window.innerHeight*.12, width: window.innerWidth, marginBottom: 20}}>
-            <div style = {{height: window.innerHeight*.12, width: window.innerWidth, position: 'fixed', background: '#01184F', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', zIndex: 100}}>
-                <Link to = '/home' style = {{marginLeft: 50, height: '10vh', width: '10vh'}}>
-                    <Image src = {Logo}/>
+    return (
+        <div style={{ height: window.innerHeight * .12, width: window.innerWidth, marginBottom: 20 }}>
+            <div style={{ height: window.innerHeight * .12, width: window.innerWidth, position: 'fixed', background: '#01184F', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', zIndex: 100 }}>
+                <Link to='/home' style={{ marginLeft: 50, height: '10vh', width: '10vh' }}>
+                    <Image src={Logo} />
                 </Link>
-                <div style = {{marginRight: 100, display: 'flex', flexDirection: 'row'}}>
-                    <NavItem label = 'Add' link = '/add'/>
-                    <NavItem label = 'View' link = '/view'/>
+                <div style={{ marginLeft: -660, display: 'flow', flexDirection: 'row', marginTop: 10 }}>
+                    <h1 style={{ color: "white", font: "Verdana" }}>QuickTrace</h1>
+                </div>
+                <div style={{ marginRight: 100, display: 'flex', flexDirection: 'row' }}>
+                    <NavItem label='Add' link='/add' />
+                    <NavItem label='View' link='/view' />
                 </div>
             </div>
         </div>
     );
 }
 
-const NavItem = ({label, link}) => {
-    return(
-        <Link to = {link} className = 'nav-item'>
+const NavItem = ({ label, link }) => {
+    return (
+        <Link to={link} className='nav-item'>
             {label}
         </Link>
     )
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 const Homepage = () => {
     return (
-        <center style = {{fontWeight: 'bold'}}>
-            QuickTrace
-        </center>
+        <div>
+            <center style={{ fontWeight: 'bold', fontSize: '40px' }}>
+                Welcome to QuickTrace!
+            </center>
+
+
+            <center style={{}}>
+                Welcome to QuickTrace, a tool to be used for contact tracing!
+            </center>
+
+            <center>
+                Select "Add" on the NavBar to fill out the survey. Or, select "View" to view the contact tracing in your area!
+            </center>
+        </div>
     )
 }
 
